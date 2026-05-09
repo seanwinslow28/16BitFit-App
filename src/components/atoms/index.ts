4e42f7bd01d5f105fc1e8bfd1dd591625876ee45
Sean Winslow <sean.winslow28@gmail.com>
2026-05-09 18:21:22 -0400

    feat(atoms): migrate 10 palette-agnostic atomic components from V3

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Atomic Components Barrel Export
+ *
+ * Wave 1: Critical Path Components (Story 1.4 Onboarding)
+ * Wave 2: Archetype Selection Components (Story 1.4 Character Selection)
+ */
+
+export {default as PixelButton} from './PixelButton';
+export {default as PixelText} from './PixelText';
+export {default as PixelInput} from './PixelInput';
+export {default as PixelSprite} from './PixelSprite';
+export {default as PixelBorder} from './PixelBorder';
+export {default as PixelIcon} from './PixelIcon';
+export {default as PixelDivider} from './PixelDivider';
+export {default as PixelBadge} from './PixelBadge';
+export {default as PixelProgressBar} from './PixelProgressBar';
+export {default as PixelCheckbox} from './PixelCheckbox';
+
+// Export types
+export type {IconName, IconSize, PixelIconProps} from './PixelIcon';
+export type {DividerThickness, PixelDividerProps} from './PixelDivider';
+export type {BadgeVariant, PixelBadgeProps} from './PixelBadge';
+export type {ProgressMode, PixelProgressBarProps} from './PixelProgressBar';
+export type {PixelCheckboxProps} from './PixelCheckbox';
